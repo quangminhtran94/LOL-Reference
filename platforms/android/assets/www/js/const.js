a73a308fe5ae5713f62ed560359bd6e9291413f2
quangminhtran94 <quangminhtran94@gmail.com>
2015-05-24 10:32:59 +0700

    delete unnecessary files to build
    delete unnecessary files to build

--- platforms/android/assets/www/js/const.js
+++
@@ -1,9 +0,0 @@
-var BASE_URL ="https://global.api.pvp.net/api/lol";
-var STATIC_DATA_CATEGORY = "/static-data";
-var NA_REGION = "/na";
-var VERSION = "/v1.2";
-var API_KEY = "api_key=9cbeeaf4-5fef-4ce8-8088-0665fb515e7d";
-var CHAMPION_CATEGORY="/champion";
-var BASE_CHAMPION_IMAGE_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/";
-var BASE_SPELL_IMAGE_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/spell/";
-var BASE_PASSIVE_IMAGE_URL = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/passive/";
