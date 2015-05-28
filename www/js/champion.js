6f051bf4f38d2f2ac59ffb2d0dc99bb796f2e2be
quangminhtran94 <quangminhtran94@gmail.com>
2015-05-28 11:15:29 +0700

    add popup for recommended items
    added popup for recommended items, showing details of item clicked.
    improve performance
    fixed minor bugs

--- www/js/champion.js
+++ www/js/champion.js
@@ -14,8 +14,7 @@ $(document).on('pageinit', "#champion-detail",function () {
 			if($(selector).length){
 				$(selector).empty();
 				$(selector).append(value);
-			}
-			
+			}			
 		});
 
 		var atkspd = (0.625/(1+result.stats.attackspeedoffset)).toFixed(2);
@@ -30,34 +29,64 @@ $(document).on('pageinit', "#champion-detail",function () {
 		var skillKeyboard = ['(Q)','(W)','(E)','(R)'];
 		$.each(result.spells, function(index, value){
 			var cost = '<i>Cost: ' + SpellToString(value, value.resource) + '</i><br>';
-			$('#champion-spells').append('<li><img src="' + BASE_SPELL_IMAGE_URL +value.image.full+'"><p>' + '<b>' + value.name + ' ' +skillKeyboard[index]+ '</b><br>' + cost + SpellToString(value, value.tooltip) +'</p></li>').listview("refresh");
+			$('#champion-spells').append('<li><img src="' + BASE_SPELL_IMAGE_URL +value.image.full+'"><p>' + '<b>' + value.name + ' ' +skillKeyboard[index%4]+ '</b><br>' + cost + SpellToString(value, value.tooltip) +'</p></li>').listview("refresh");
 		});
 		$('#champion-spells li p').css("white-space","normal");
-	}}); 
-	$('#champion-recommend').bind('collapsibleexpand', function(event, ui){
+	}});
+
+var isItemsLoaded = false; 
+$('#champion-recommend').bind('collapsibleexpand', function(event, ui){
+	if(!isItemsLoaded){
 		$('#recommended-items').empty();
 		$.ajax({url: BASE_URL + STATIC_DATA_CATEGORY + NA_REGION + VERSION + CHAMPION_CATEGORY + "/"+id+"?"+LOCATION+"="+language+ "&champData=recommended&"+API_KEY, success: function(result){
 			$.each(result.recommended, function(index, value){
-				if(value.map =="SR" && value.mode == "CLASSIC"){
-					$.each(value.blocks, function(index, value){
-						var type = value.type;
-						$("#recommended-items").append("<li id='"+value.type+"'><p><b id='"+value.type+"-title'></b></p></li>");
-						changeLang(language);
-						$.each(value.items, function(index, value){
-							var numberOfItem = value.count;
-							$.ajax({url: BASE_URL + STATIC_DATA_CATEGORY + NA_REGION + VERSION + ITEM_CATEGORY + "/"+value.id+"?"+LOCATION+"="+language+ "&itemData=image&"+API_KEY , success: function(result){
-								for(i = 0; i < numberOfItem; i++){
-									$("#"+type).append('<img src="' + BASE_ITEM_IMAGE_URL + result.image.full +'">');
-								}	
-							}});
-						});
-					});
+				if((value.map =="SR"|| value.map=="1") && value.mode == "CLASSIC"){
+					displayRecommendedItems(value.blocks);
+					isItemsLoaded = true;
 				}
 			});
+			if(!isItemsLoaded){
+				$.each(result.recommended, function(index, value){
+					if( value.map=="10" && value.mode == "CLASSIC"){
+						displayRecommendedItems(value.blocks);
+						isItemsLoaded = true;
+					}
+				});
+			}
 		}});
-	});
+
+	}
+
+});
+
+$(document).on("click", ".item-image", function(){
+	var id = $(this).attr('id');
+	$("#ItemPopup p").empty();
+	$.ajax({url: BASE_URL + STATIC_DATA_CATEGORY + NA_REGION + VERSION + ITEM_CATEGORY + "/"+id+"?"+LOCATION+"="+language+ "&itemData=gold&"+API_KEY, success: function(result){
+		$("#ItemPopup p").append('<b>'+result.name+'</b><br>');
+		$("#ItemPopup p").append(result.description);
+	}});
+	$("#ItemPopup").popup("open");
+})
+
 });  
 
+function displayRecommendedItems(blocks){
+	$.each(blocks, function(index, value){
+		var type = value.type.toLowerCase();
+		$("#recommended-items").append("<li id='"+type+"'><p><b id='"+type+"-title'></b></p></li>");
+		changeLang(language);
+		$.each(value.items, function(index, value){
+			var numberOfItem = value.count;
+			$.ajax({url: BASE_URL + STATIC_DATA_CATEGORY + NA_REGION + VERSION + ITEM_CATEGORY + "/"+value.id+"?"+LOCATION+"="+language+ "&itemData=image&"+API_KEY , success: function(result){
+				for(i = 0; i < numberOfItem; i++){
+					var selector='#'+type.toLowerCase();
+					$(selector).append('<img class="item-image" id="'+result.id+'" src="' + BASE_ITEM_IMAGE_URL + result.image.full +'">');
+				}	
+			}});
+		});
+	});
+}
 
 function SpellToString(spell, str){
 	var start=0;
@@ -66,47 +95,47 @@ function SpellToString(spell, str){
 		str = "Azir summons an untargetable Sand Soldier for 9 seconds. When Azir attacks an enemy in a soldier's range, the soldier attacks instead of Azir - dealing 50 / 55 / 60 / 65 / 70 / 75 / 80 / 85 / 90 / 95 / 100 / 110 / 120 / 130 / 140 / 150 / 160 / 170 (45 + 5 / 10 at each level) (+ 60% AP) magic damage to all enemies in a line. If multiple soldiers strike the same target, each soldier after the first deals 25% damage.<br> Azir can store up to 2 Sand Soldiers at a time, but there is no hard limit on the number of Sand Soldiers that can be active on the field at once. Sand Soldiers expire twice as fast while within range of an enemy turret, and will expire instantly if Azir moves too far away.<br>	Sand Soldiers cannot attack enemy structures, but can be summoned directly on top of an enemy turret to sacrifice it and deal 60 / 70 / 80 / 90 / 100 / 110 / 120 / 130 / 140 / 150 / 160 / 170 / 180 / 190 / 200 / 210 / 220 / 230 (50 + 10 × Azir's level) (+ 40% AP) magic damage to the turret.";
 	}else{
 		while(str.indexOf('{{', start) != -1){
-		var index = str.indexOf('{{', start);
-		switch(str[index+3]){
-			case 'e':
-			case 'f':
-			var num = parseInt(str[index+4]);
-			str = str.replace('{{ '+str[index+3]+str[index+4],spell.effectBurn[num]);
-			str = str.replace(' }}','');
-			break;
-			case 'a':
-			var key = str.substring(index+3, index+5);
-			for(i=0; i<spell.vars.length;i++){
-				if(spell.vars[i].key == key){
-					var replace = spell.vars[i].coeff[0];
-					switch(spell.vars[i].link){
-						case "attackdamage":
-						replace += ' AD';
-						break;
-						case "spelldamage":
-						replace += ' AP';
-						break;
-						case "bonusattackdamage":
-						replace += ' bonus AD';
-						break;
-						case "bonusspelldamage":
-						replace += ' bonus AP';
-						break;
-						default:
+			var index = str.indexOf('{{', start);
+			switch(str[index+3]){
+				case 'e':
+				case 'f':
+				var num = parseInt(str[index+4]);
+				str = str.replace('{{ '+str[index+3]+str[index+4],spell.effectBurn[num]);
+				str = str.replace(' }}','');
+				break;
+				case 'a':
+				var key = str.substring(index+3, index+5);
+				for(i=0; i<spell.vars.length;i++){
+					if(spell.vars[i].key == key){
+						var replace = spell.vars[i].coeff[0];
+						switch(spell.vars[i].link){
+							case "attackdamage":
+							replace += ' AD';
+							break;
+							case "spelldamage":
+							replace += ' AP';
+							break;
+							case "bonusattackdamage":
+							replace += ' bonus AD';
+							break;
+							case "bonusspelldamage":
+							replace += ' bonus AP';
+							break;
+							default:
+							break;
+						}
+						str = str.replace('{{ a'+str[index+4],replace);
+						str = str.replace(' }}','');
 						break;
 					}
-					str = str.replace('{{ a'+str[index+4],replace);
-					str = str.replace(' }}','');
-					break;
 				}
+				break;
+				default:
+				break;
 			}
-			break;
-			default:
-			break;
-		}
-		start = index;
+			start = index;
 
-	}
+		}
 	}
 	
 	return str;
